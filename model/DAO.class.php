86ed436f534080665fab8b94f69eed6adc8939db
Vincent AUBRIOT <vincent3897@gmail.com>
2017-10-16 10:08:24 +0200

    Save changes

--- model/DAO.class.php
+++ model/DAO.class.php
@@ -93,16 +93,16 @@ class DAO {
     function updateRSS(RSS $rss) {
         // Met à jour uniquement le titre et la date
         $titre = $this->db->quote($rss->titre());
-        $q = "UPDATE RSS SET titre=:titre, date=':date' WHERE url=':url'";
+        $q = "UPDATE RSS SET titre=:titre, date=:d WHERE url=:url";
         try {
             $r = $this->db->prepare($q);
-            $r->bindParam(":titre", $rss->date());
-            $r->bindParam(":date", $rss->date());
-            $r->bindParam(":url", $rss->url());
+            $titre = $rss->titre;
+            $url = $rss->url;
+            $date = time();
+            $r->bindParam(":titre", $titre);
+            $r->bindParam(":d", $date);
+            $r->bindParam(":url", $url);
             $r->execute();
-            if ($r == 0) {
-                die("updateRSS error: no rss updated\n");
-            }
         } catch (PDOException $e) {
             die("PDO Error :".$e->getMessage());
         }
