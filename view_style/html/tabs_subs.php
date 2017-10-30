a3c624adb20b3f9f3e1bd686991598f14b2fe843
Habib <habib.slim.38@gmail.com>
2017-10-30 09:19:29 +0100

    Correction d'une typo

--- view_style/html/tabs_subs.php
+++ view_style/html/tabs_subs.php
@@ -4,15 +4,15 @@
         <i class = "fa fa-rss"></i>
         Mes abonnements</a>
     </li>
-    <li <?= $tab_mode=="catT" ? 'class="active"' : ""?>>
-        <a href="afficher_cat.ctrl.php">
-        <i class = "fa fa-newspaper-o"></i>
-        Par catégories</a>
-    </li>
     <li <?= $tab_mode=="subT" ? 'class="active"' : ""?>>
         <a href="manage_subs.ctrl.php">
         <i class = "fa fa-magnet"></i>
         S'abonner</a>
+    </li>
+    <li <?= $tab_mode=="catT" ? 'class="active"' : ""?>>
+        <a href="afficher_cat.ctrl.php">
+        <i class = "fa fa-newspaper-o"></i>
+        Par catégories</a>
     </li>
     <li <?= $tab_mode=="picT" ? 'class="active"' : ""?>>
         <a href="afficher_mozaic_cat.ctrl.php">
